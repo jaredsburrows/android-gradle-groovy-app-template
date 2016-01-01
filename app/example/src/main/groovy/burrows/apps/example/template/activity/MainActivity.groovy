e2bec3f4de822c59ff079562295f136edb625cce
Jared Burrows <jaredsburrows@gmail.com>
2016-01-01 01:31:22 -0500

    Fix

--- app/example/src/main/groovy/burrows/apps/example/template/activity/MainActivity.groovy
+++ app/example/src/main/groovy/burrows/apps/example/template/activity/MainActivity.groovy
@@ -1,16 +1,17 @@
-package burrows.apps.example.template.activity;
+package burrows.apps.example.template.activity
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity
+import android.os.Bundle
+import burrows.apps.example.template.R
 
-import burrows.apps.example.template.R;
 //import burrows.apps.example.template.fragment.PlaceholderFragment;
 
 /**
  * @author <a href="mailto:jaredsburrows@gmail.com">Jared Burrows</a>
  * @since 0.0.1
  */
-public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
