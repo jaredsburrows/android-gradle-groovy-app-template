0405f1086597b2b797ec7bdb9215a78c68921907
Jared Burrows <jaredsburrows@gmail.com>
2016-07-16 18:26:23 -0700

    updates

--- src/test/groovy/burrows/apps/example/template/activity/MainActivitySpec.groovy
+++ src/test/groovy/burrows/apps/example/template/activity/MainActivitySpec.groovy
@@ -1,14 +1,17 @@
 package burrows.apps.example.template.activity
 
+import android.os.Build
+import burrows.apps.example.template.BuildConfig
 import org.robolectric.Robolectric
+import org.robolectric.annotation.Config
 import org.robospock.GradleRoboSpecification
 
 /**
  * @author <a href="mailto:jaredsburrows@gmail.com">Jared Burrows</a>
  * @since 0.0.1
  */
+@Config(constants = BuildConfig, sdk = Build.VERSION_CODES.LOLLIPOP)
 class MainActivitySpec extends GradleRoboSpecification {
-
 
     def "activity should not be null"() {
         when:
