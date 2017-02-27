efdab84e0c6ccb7b4474301243d34863e101a8f8
Jared Burrows <jaredsburrows@gmail.com>
2017-02-26 23:11:32 -0800

    updates

--- src/main/groovy/burrows/apps/example/template/activity/MainActivity.groovy
+++ src/main/groovy/burrows/apps/example/template/activity/MainActivity.groovy
@@ -1,5 +1,6 @@
 package burrows.apps.example.template.activity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import burrows.apps.example.template.R
@@ -7,11 +8,12 @@ import burrows.apps.example.template.R
 /**
  * @author <a href="mailto:jaredsburrows@gmail.com">Jared Burrows</a>
  */
-public class MainActivity extends AppCompatActivity {
+@SuppressLint("NewApi")
+class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_main)
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
